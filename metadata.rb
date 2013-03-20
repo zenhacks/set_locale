39df80afbcb25d8fb9c7a67bed193291dfb77bd4
Steven Yang <yangchenyun@gmail.com>
2013-03-20 12:19:10 +0800

    updated metadata.rb to include version

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,10 @@
 name "set_locale"
-description "A cookbook to set locales (default to en_US-UTF8) to enable using UTF8 databases with PostgreSQL server"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-licence "Apache 2.0"
 maintainer "Steven Yang"
 maintainer_email "yangchenyun@gmail.com"
+licence "Apache 2.0"
+description "A cookbook to set locales (default to en_US-UTF8) to enable using UTF8 databases with PostgreSQL server"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '0.1.0'
 recipe "default", "set the system locales to en_US.UTF-8"
 %w{ debian ubuntu }.each do |os|
   supports os
