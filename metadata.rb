2eb750a90d6218c3273342b70b20e01d39907a35
Steven Yang <yangchenyun@gmail.com>
2013-03-20 12:16:42 +0800

    correct syntax error in metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,10 @@
-name "set_locale",
-description "A cookbook to set locales (default to en_US-UTF8) to enable using UTF8 databases with PostgreSQL server",
+name "set_locale"
+description "A cookbook to set locales (default to en_US-UTF8) to enable using UTF8 databases with PostgreSQL server"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-maintainer "Steven Yang",
-maintainer_email "yangchenyun@gmail.com",
+licence "Apache 2.0"
+maintainer "Steven Yang"
+maintainer_email "yangchenyun@gmail.com"
+recipe "default", "set the system locales to en_US.UTF-8"
 %w{ debian ubuntu }.each do |os|
   supports os
 end
